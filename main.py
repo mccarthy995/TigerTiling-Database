2a2f628ae18d4ce667c3c0058e190f11fc151395
mccarthy995 <53656981+mccarthy995@users.noreply.github.com>
2023-04-06 18:25:32 +0000

    added login, imported icons, changed template, added client site and plot

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(
 	static_folder='static')
 
 @app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
 
