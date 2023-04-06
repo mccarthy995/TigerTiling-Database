9d316e69910a9d746a042bd9d40fc4f8b464006d
mccarthy995 <53656981+mccarthy995@users.noreply.github.com>
2023-04-06 12:58:05 +0000

    File structure added, downloaded html template, downloaded css, fixed broken links

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(
+	__name__,
+	template_folder='templates',
+	static_folder='static')
+
+@app.route("/")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/spare")
+def spare():
+    return render_template("sparetemplate.html")
+
+if __name__ == "__main__":
+	app.run(
+		host='0.0.0.0',
+		debug=True
+	)
